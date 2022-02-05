08ead49020307d43f194095ea274c47c3b051d98
J-Pablo4 <73197689+J-Pablo4@users.noreply.github.com>
2022-02-04 20:44:31 -0600

    Update cve.sh

--- cve.sh
+++ cve.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-sudo git clone https://github.com/scipag/vulscan scipag_vulscan
-sudo ln -s `pwd`/scipag_vulscan /usr/share/nmap/scripts/vulscan
 cd /usr/share/nmap/scripts/
 sudo git clone https://github.com/vulnersCom/nmap-vulners.git
 cd ~/Desktop
